dc9d886e92767373d4a88dc17b3adcfd2b71ec1e
JaeinChoii <hightreehuman@gmail.com>
2022-12-12 12:25:25 +0900

    feat: Todo 수정 및 삭제 기능 구현

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -5,10 +5,10 @@ const router = express.Router();
 // 기본주소: localhost:PORT/
 
 // GET localhost:PORT/todos - show all todos (READ)
-router.get('/todos', async(req, res) => {
-  // let data = await Todo.findAll();
-  // console.log(data);
-  // res.send(data);
+router.get('/todos', async (req, res) => {
+  // Todo.findAll().then((data) => {
+  //   res.send(data);
+  // });
 
   try {
     let data = await Todo.findAll();
@@ -22,10 +22,67 @@ router.get('/todos', async(req, res) => {
 router.post('/todo', async (req, res) => {
   try {
     let newTodo = await Todo.create({
-      // id는 auto_increment, done은 default 존재
       title: req.body.title,
     });
     res.send(newTodo);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+// PATCH localhost:PORT/todo/:todoId - edit a specific todo (UPDATE)
+// 수정 성공시; true -> res.send(true)
+// 수정 실패시; false -> res.send(false)
+router.patch('/todo/:todoId', async (req, res) => {
+  // console.log(req.body); // { title: 'my todo - 수정', done: true }
+  // console.log(req.params); // { todoId: '1' }
+  try {
+    let [isUpdated] = await Todo.update(
+      {
+        title: req.body.title,
+        done: req.body.done,
+      },
+      {
+        where: {
+          id: req.params.todoId,
+        },
+      }
+    );
+    // console.log(isUpdated);
+    // 수정 성공시; [ 1 ] -> 1
+    // 수정 실패시; [ 0 ] -> 0
+
+    // 수정 실패 (!0)
+    if (!isUpdated) {
+      return res.send(false);
+    }
+
+    // 수정 성공
+    res.send(true);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+// DELETE localhost:PORT/todo/:todoId - remove a specific todo (DELETE)
+router.delete('/todo/:todoId', async (req, res) => {
+  // console.log(req.params); // { todoId: '8' }
+
+  try {
+    let isDeleted = await Todo.destroy({
+      where: {
+        id: req.params.todoId,
+      },
+    });
+    // console.log(isDeleted); // 1 or 0
+
+    // 삭제 실패 (!0)
+    if (!isDeleted) {
+      return res.send(false);
+    }
+
+    // 삭제 성공
+    res.send(true);
   } catch (err) {
     res.send(err);
   }
