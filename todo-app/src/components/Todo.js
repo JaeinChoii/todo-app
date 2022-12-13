f67be8c2c4555c077daf1184d1468da1fc371ca9
JaeinChoii <hightreehuman@gmail.com>
2022-12-13 12:01:58 +0900

    feat: Todo 삭제 기능 구현

--- todo-app/src/components/Todo.js
+++ todo-app/src/components/Todo.js
@@ -1,5 +1,13 @@
-const Todo = ({item}) => {
+import { useState } from 'react';
+
+const Todo = ({ item, deleteItem }) => {
+  // console.log(item); // { id: 1, title: 'todo1', done: false, }
   const { id, title, done } = item;
+  const [todoItem, setTodoItem] = useState(item);
+
+  const onDeleteBtnClick = () => {
+    deleteItem(todoItem);
+  };
 
   return (
     <div className="Todo">
@@ -8,8 +16,10 @@ const Todo = ({item}) => {
         id={`todo${id}`}
         name={`todo${id}`}
         value={`todo${id}`}
-        defaultChecked={done} />
+        defaultChecked={done}
+      />
       <label htmlFor={`todo${id}`}>{title}</label>
+      <button onClick={onDeleteBtnClick}>DELETE</button>
     </div>
   );
 };
